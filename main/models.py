11cd252012b8491f1b339a058c73a8831977dcd9
Juan Carlos <juancagb.17@gmail.com>
2016-05-13 09:33:05 -0500

    Planilla model and app added

--- main/models.py
+++ main/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Planilla(models.Model):
+	idPlanilla = models.IntegerField()
+	idAfp = models.CharField(max_length=18)
+	idTrabajador= models.CharField(max_length=18)
+	idMes = models.CharField(max_length=18)
+	DiasFalta = models.CharField(max_length=18)
+	HorasFalta = models.CharField(max_length=18)
+	TotalIngresos = models.DecimalField(max_digits=7, decimal_places=2)
+	TotalDescuentos = models.DecimalField(max_digits=7, decimal_places=2)
