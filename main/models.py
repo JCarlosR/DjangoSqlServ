422b4fe23e04738be7bd3d5b2fb903a77ef526e9
Juan Carlos <juancagb.17@gmail.com>
2016-05-18 23:29:36 -0500

    Using views instead of tables

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Planilla(models.Model):
 	idPlanilla = models.IntegerField(primary_key=True)
 	idAfp = models.CharField(max_length=18)
-	idTrabajador= models.CharField(max_length=18)
+	idTrabajador= models.CharField(max_length=37)
 	idMes = models.CharField(max_length=18)
 	DiasFalta = models.CharField(max_length=18)
 	HorasFalta = models.CharField(max_length=18)
@@ -13,13 +13,13 @@ class Planilla(models.Model):
 	TotalDescuentos = models.DecimalField(max_digits=7, decimal_places=2)
 
 	class Meta:
-		db_table = 'Planilla'
+		db_table = 'V_Planillas'
 
 class Detalle(models.Model):
 	idPlanilla = models.IntegerField(primary_key=True)
-	idConcepto = models.IntegerField(primary_key=True)
+	idConcepto = models.CharField(max_length=18)
 	Monto = models.DecimalField(max_digits=7, decimal_places=2)
 	idMovimiento = models.IntegerField()
 
 	class Meta:
-		db_table = 'DetaPlanilla'
+		db_table = 'V_DetaPlanilla'
